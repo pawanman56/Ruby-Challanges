4eaa5305e01ad5eac84d861bb3aedcc6fb55f64f
pawanman56 <pawanman56@gmail.com>
2017-11-04 23:51:52 +0545

    it block elaborated on RSpec

--- 9_change/spec/change_spec.rb
+++ 9_change/spec/change_spec.rb
@@ -3,21 +3,21 @@ require 'rspec'
 
 describe Change do
   context 'is not a valid amount' do
-    it 'return false' do
+    it 'should return false on string input' do
       expect(Change.new("amount").change).to eq(false)
     end
 
-    it 'return false' do
+    it 'should return false on negative value' do
       expect(Change.new(-5).change).to eq(false)
     end
   end
 
   context 'is a valid amount' do
-    it 'return true' do
+    it 'should return true on valid amount' do
       expect(Change.new(56).change).to eq([50, 5, 1])
     end
 
-    it 'return true' do
+    it 'should return true on valid large amount' do
       expect(Change.new(527).change).to eq([100, 100, 100, 100, 100, 20, 5, 1, 1])
     end
   end
