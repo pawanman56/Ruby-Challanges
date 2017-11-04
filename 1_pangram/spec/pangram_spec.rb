4c5354e0710ba2427b65c59ef7095fe84de7f53d
pawanman56 <pawanman56@gmail.com>
2017-11-04 11:07:44 +0545

    it block ilaborated

--- 1_pangram/spec/pangram_spec.rb
+++ 1_pangram/spec/pangram_spec.rb
@@ -3,21 +3,21 @@ require 'rspec'
 
 describe Pangram do 
   context 'sentence count is less than 26' do
-    it 'returns false' do
+    it 'should returns false on sentence of less count' do
       expect(Pangram.new('abc').pangram?).to eq(false)
     end
   end
 #  
   context 'sentence count is greater than 26' do
-    it 'return false' do
+    it 'should return false on incomplete letters of alphabet' do
       expect(Pangram.new('abcdefghijklmnopqrstuvwxyy').pangram?).to eq(false)
     end
 #
-    it 'return true' do
+    it 'should return true on all letters included of alphabet' do
       expect(Pangram.new('The quick brown fox jumps over the lazy dog').pangram?).to eq(true)
     end
 
-    it 'return true' do
+    it 'should return false on incorrect input' do
       expect(Pangram.new('something').pangram?).to eq(false)
     end
   end
