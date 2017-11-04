2d6a9880d9fa963f643af7ac826bb3aa4f3a64ed
pawanman56 <pawanman56@gmail.com>
2017-11-04 20:01:34 +0545

    RSpec file fixed and elaborated

--- 6_anagram/spec/anagram_spec.rb
+++ 6_anagram/spec/anagram_spec.rb
@@ -3,25 +3,25 @@ require 'rspec'
 
 describe Anagram do
   context 'is not a valid word' do
-    it 'return false' do
+    it 'should return false on numbers input' do
       expect(Anagram.new(123, 456).anagram).to eq(false)
     end
   end
 
   context 'is a valid word' do
-    it 'return false' do
+    it 'should return false on inputs of different data types' do
       expect(Anagram.new("word", 456).anagram).to eq(false)
     end
 
-    it 'return false' do
+    it 'should return false on empty or incomplete inputs' do
       expect(Anagram.new("word", []).anagram).to eq(false)
     end
 
-    it 'return false' do
+    it 'should return false on wrong candidates' do
       expect(Anagram.new("word", ["apple", "ball"]).anagram).to eq(false)
     end
 
-    it 'return true' do
+    it 'should return true on correct candidates' do
       expect(Anagram.new("anagram", ["apple", "ball", "nagaram"]).anagram).to eq(true)
     end
   end
