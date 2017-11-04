04006d765a17217ebc39cc676876b33cf6dda77f
pawanman56 <pawanman56@gmail.com>
2017-11-05 01:53:08 +0545

    RSpec written for Pythagorean triplets generator

--- 10_pythagorean/lib/pythagorean.rb
+++ 10_pythagorean/lib/pythagorean.rb
@@ -1,16 +1,23 @@
 class Pythagorean
 
-  def pythagorean(n)
-    for x in 1..n
+  def initialize(number)
+    @n = number
+  end
+
+  def pythagorean
+    return false if (@n.is_a? Numeric) == false
+    return false if @n > 1000
+
+    for x in 1..@n
       y = x+1
       z = y+1
-      while z <= n
+      while z <= @n
         while z * z < x * x + y * y
           z = z + 1
         end
-        if z * z == x * x + y * y and z <= n and x + y + z == 1000 then
-          puts ("h = #{z}, p = #{x}, b = #{y}")
-          puts ("h*p*b = #{z*x*y}")
+        if z * z == x * x + y * y and z <= @n and x + y + z == 1000 then
+          #puts ("a = #{x}, b = #{y}, c = #{z}")
+          return ("Product = #{x*y*z}")
         end
         y = y + 1
       end
--- 10_pythagorean/spec/pythagorean_spec.rb
+++ 10_pythagorean/spec/pythagorean_spec.rb
@@ -2,5 +2,19 @@ require_relative '../lib/pythagorean.rb'
 require 'rspec'
 
 describe Pythagorean do 
-  
+  context 'is not a valid number' do
+    it 'should return false on string input' do
+      expect(Pythagorean.new("string").pythagorean).to eq(false)
+    end
+  end
+
+  context 'is a valid number' do
+    it 'should return false on number greater than 1000' do
+      expect(Pythagorean.new(1001).pythagorean).to eq(false)
+    end
+
+    it 'should return true on correct input' do
+      expect(Pythagorean.new(567).pythagorean).to eq("Product = 31875000")
+    end
+  end
 end
