16875f8897e8fee15f4907169be9fb43e448204c
pawanman56 <pawanman56@gmail.com>
2017-11-04 13:25:30 +0545

    it block elaborated

--- 4_leap_year/spec/leap_year_spec.rb
+++ 4_leap_year/spec/leap_year_spec.rb
@@ -3,21 +3,21 @@ require 'rspec'
 
 describe LeapYear do
   context 'is not a valid year' do
-    it 'return false' do
+    it 'should return false on strings' do
       expect(LeapYear.new('string').leap).to eq(false)
     end
 
-    it 'return false' do
+    it 'should return false on negative values' do
       expect(LeapYear.new(-3123).leap).to eq(false)
     end
   end
 
   context 'is a valid year' do
-    it 'return false' do
+    it 'should return false on normal years' do
       expect(LeapYear.new(1990).leap).to eq(false)
     end
 
-    it 'return true' do
+    it 'should return true on leap years' do
       expect(LeapYear.new(1996).leap).to eq(true)
     end
   end
