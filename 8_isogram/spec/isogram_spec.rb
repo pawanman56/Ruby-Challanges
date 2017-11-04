883512c41c520f13d12b0199ebfbd75536a49f07
pawanman56 <pawanman56@gmail.com>
2017-11-04 20:27:21 +0545

    it block elaborated

--- 8_isogram/spec/isogram_spec.rb
+++ 8_isogram/spec/isogram_spec.rb
@@ -3,17 +3,17 @@ require 'rspec'
 
 describe Isogram do 
   context 'is not a valid word' do
-    it 'return false' do
+    it 'should return false on numeric inputs' do
       expect(Isogram.new(123).isogram).to eq(false)
     end
   end
 
   context 'is a valid word' do
-    it 'return false' do
+    it 'should return false on word that is not isogram' do
       expect(Isogram.new("apple").isogram).to eq(false)
     end
 
-    it 'return true' do
+    it 'should return true on an isogram' do
       expect(Isogram.new("subdermatoglyphic").isogram).to eq(true)
     end
   end
