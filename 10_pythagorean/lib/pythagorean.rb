e083a1294e126f65b95613d3f8216ec9101b42a3
pawanman56 <pawanman56@gmail.com>
2017-11-05 01:14:35 +0545

    Pythagorean triplets generator

--- 10_pythagorean/lib/pythagorean.rb
+++ 10_pythagorean/lib/pythagorean.rb
@@ -1,7 +1,20 @@
 class Pythagorean
 
-  def pythagoras(n)
-    
+  def pythagorean(n)
+    for x in 1..n
+      y = x+1
+      z = y+1
+      while z <= n
+        while z * z < x * x + y * y
+          z = z + 1
+        end
+        if z * z == x * x + y * y and z <= n and x + y + z == 1000 then
+          puts ("h = #{z}, p = #{x}, b = #{y}")
+          puts ("h*p*b = #{z*x*y}")
+        end
+        y = y + 1
+      end
+    end
   end
 
 end
