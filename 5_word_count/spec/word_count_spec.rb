0bd635c443ce67abc182c981d2e447b631c86bff
pawanman56 <pawanman56@gmail.com>
2017-11-04 13:41:43 +0545

    it block elaborated

--- 5_word_count/spec/word_count_spec.rb
+++ 5_word_count/spec/word_count_spec.rb
@@ -3,26 +3,22 @@ require 'rspec'
 
 describe WordCount do
   context 'is not a valid word' do
-    it 'return false' do
+    it 'should return false on number' do
       expect(WordCount.new(123).counter).to eq(false)
     end
   end
 
   context 'is a valid word' do
-    it 'return false' do
+    it 'should return false on empty input' do
       expect(WordCount.new('').counter).to eq(false)
     end
 
-    it 'return true' do
+    it 'should return true on a word' do
       expect(WordCount.new('this').counter).to eq(true)
     end
 
-    it 'return true' do
+    it 'should return true on a sentence' do
       expect(WordCount.new('This is joy, this is summer. Keep alive.').counter).to eq(true)
-    end
-
-    it 'return true' do
-      expect(WordCount.new(789).counter).to eq(false)
     end
   end
 end
