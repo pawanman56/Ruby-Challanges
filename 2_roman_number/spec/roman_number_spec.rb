472c6ac58a3c09a9c355af11cd15650987a5b8a7
pawanman56 <pawanman56@gmail.com>
2017-11-04 11:23:23 +0545

    it block elaborated

--- 2_roman_number/spec/roman_number_spec.rb
+++ 2_roman_number/spec/roman_number_spec.rb
@@ -1,28 +1,20 @@
-require_relative '../lib/roman_numbers.rb'
+require_relative '../lib/roman_number.rb'
 require 'rspec'
 
 describe RomanNumber do
   context 'input is not a number' do
-    it 'returns false' do
+    it 'should return false on wrong input' do
       expect(RomanNumber.new('alphabets').to_roman).to eq(false)
     end
   end
 
   context 'input is a number' do
-    it 'returns false' do
+    it 'should return false on negative numbers' do
       expect(RomanNumber.new(-123).to_roman).to eq(false)
     end
   
-    it 'return true' do
+    it 'should return true on valid numbers' do
       expect(RomanNumber.new(123).to_roman).to eq(true)
-    end
-
-    it 'return true' do
-      expect(RomanNumber.new('string').to_roman).to eq(false)
-    end
-
-    it 'return true' do
-      expect(RomanNumber.new(-567).to_roman).to eq(false)
     end
   end
 end
